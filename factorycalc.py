9a305a63aacb578d04e557076a0bdf4ddcfe5c56
Schm000L <darklord_dragonslayer@hotmail.com>
2022-03-17 17:47:46 +0100

    Comments

--- factorycalc.py
+++ factorycalc.py
@@ -1,13 +1,7 @@
-
-# from enum import Enum
-# ItemIDs = Enum('ItemID', 'Iron_Ore Copper_Ore Caterium_Ore Bauxite Quartz Sulfur Oil Water')
-
-# class Item:
-#     sharedbyall = 'all'         # class variable shared by all instances
-
-#     def __init__(self, id):
-#         self.id = id    # instance variable unique to each instance
-
+# Calculates how many machines and resources are needed to produce given products
+# Starts in main()
+# Set goal to whatever products you wish to produce
+# Output in out.json
 
 import json
 
@@ -17,17 +11,21 @@ to_make = {}
 
 def handle_requirements(goal_name, goal_amount, level,  recipe_version = "default"):     
             if goal_name in RECIPES:
+                # goal_recipe expected to look like: 
+                # {"production": ##, "requirements": {"RESOURCE1": ##, ...},"machine": "MACHINE_NAME","by_products": {"BYPROUCT1": ##, ...}}
                 goal_recipe = RECIPES[goal_name][recipe_version]
-                goal_production_ratio = goal_amount/goal_recipe["production"]
-                tabs = " "*level
+                goal_production_ratio = goal_amount/goal_recipe["production"] # how much needed over how much one machine can produce
+                tabs = " "*level # for a nicer print
                 print(tabs,"Need", round(goal_production_ratio, 2), goal_name, goal_recipe["machine"]+"(s) for", goal_amount, "per min")
                 if goal_recipe["requirements"] is not None:
-                    for r_key, r_val in goal_recipe["requirements"].items():
-                        rec_version = "alt1" if r_key == "Screw" else "default"
+                    for r_key, r_val in goal_recipe["requirements"].items(): # loop through each requirement (get name/id and amount needed)
+                        rec_version = "alt1" if r_key == "Screw" else "default" # Want to use alt1 recipe instead of default for all Screw production
                         requirement_recipe = RECIPES[r_key][rec_version]
 
                         r_goal = r_val*goal_production_ratio
                         num_machines = r_goal/requirement_recipe["production"]
+
+                        # Add required product to the list if new, else update values (may save on machines if overlapping requirements)
                         if r_key not in to_make:
                             to_make[r_key] = {"total": r_goal, "machine": requirement_recipe["machine"], "num_machines": num_machines ,"for_"+goal_name: r_goal}
                         else:
@@ -38,6 +36,8 @@ def handle_requirements(goal_name, goal_amount, level,  recipe_version = "defaul
                                 to_make[r_key]["for_"+goal_name] = r_goal
                             else:
                                 to_make[r_key]["for_"+goal_name] += r_goal
+                        
+                        # Add by products to the list
                         if "by_products" in requirement_recipe and requirement_recipe["by_products"] is not None:
                             for by_key, by_val in requirement_recipe["by_products"].items():
                                 if by_key not in to_make:
@@ -56,9 +56,8 @@ def handle_requirements(goal_name, goal_amount, level,  recipe_version = "defaul
                 print("Could not find recipe for", goal_name)
 
 
-
-
 def main():
+    # Set goal to whatever products you wish to produce
     goal = {"Screw": 40}
     
     global to_make 
@@ -81,8 +80,6 @@ def main():
 
     print(to_make)
             
-
-
 
 if __name__ == "__main__":
     main()
